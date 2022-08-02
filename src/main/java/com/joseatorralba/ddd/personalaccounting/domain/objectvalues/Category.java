94735507ed57018eaa6d1607c6eb1db2195eb71c
Jose Antonio Torralba <jatorralba.dev@gmail.com>
2022-08-02 17:21:33 +0200

    Adding Account Aggregate to model

---
+++ src/main/java/com/joseatorralba/ddd/personalaccounting/domain/objectvalues/Category.java
@@ -0,0 +1,15 @@
+package com.joseatorralba.ddd.personalaccounting.domain.objectvalues;
+
+public enum Category {
+	MARKET,
+	HOUSE,
+	CAR,
+	SUPPLIES,
+	CLOTHING,
+	ELECTRONICS,
+	HEALTH,
+	SPORT,
+	LEISURE,
+	HOLLIDAYS,
+	GIFTS
+}
---
+++ src/main/java/com/joseatorralba/ddd/personalaccounting/domain/objectvalues/Entry.java
@@ -0,0 +1,17 @@
+package com.joseatorralba.ddd.personalaccounting.domain.objectvalues;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class Entry {
+
+	LocalDate date;
+	String description;
+	double amount;
+	Category category;
+	
+}
